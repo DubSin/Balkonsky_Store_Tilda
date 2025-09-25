2065634177690c7c084cad1b6fb5a4083456dcb6
DubSin <95424344+DubSin@users.noreply.github.com>
2025-09-25 18:44:59 +0300

    Update menu_keyboard.py

--- keyboards/menu_keyboard.py
+++ keyboards/menu_keyboard.py
@@ -5,7 +5,8 @@ from aiogram.types import WebAppInfo
 def get_menu_kb() -> InlineKeyboardMarkup:
     kb = InlineKeyboardBuilder()
     kb.button(text="Курс Юаня", callback_data='yuan_course')
-    kb.button(text="Магазин", web_app=WebAppInfo(url='https://project14286403.tilda.ws/'))
+    kb.button(text="Магазин", web_app=WebAppInfo(url='https://balkonsky-store.ru'))
     kb.button(text="Кулькулятор валют", callback_data='currency_calculator')
     kb.adjust(2)
+
     return kb.as_markup(resize_keyboard=True)
